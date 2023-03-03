3ad6ea128e7fd5d3a64311719d0a8e116bb3c3d0
deceitya <49361292+deceitya@users.noreply.github.com>
2023-03-03 18:28:07 +0900

    面積の計算

--- src/main/kotlin/mc/tsukimiya/mooney/land/domain/Coordinate.kt
+++ src/main/kotlin/mc/tsukimiya/mooney/land/domain/Coordinate.kt
@@ -5,4 +5,13 @@ data class Coordinate(val minX: Int, val minZ: Int, val maxX: Int, val maxZ: Int
         require(minX <= maxX)
         require(minZ <= maxZ)
     }
+
+    /**
+     * 面積を計算
+     *
+     * @return 縦×横
+     */
+    fun calcArea(): Int {
+        return (maxX - minX + 1) * (maxZ - minZ + 1)
+    }
 }
