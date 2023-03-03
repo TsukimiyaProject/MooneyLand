e593f7fe8147c7e27c86e6308df69c1f8093bbc5
deceitya <49361292+deceitya@users.noreply.github.com>
2023-03-03 22:43:56 +0900

    createとfindByOwner追加

--- src/main/kotlin/mc/tsukimiya/mooney/land/domain/LandRepository.kt
+++ src/main/kotlin/mc/tsukimiya/mooney/land/domain/LandRepository.kt
@@ -1,5 +1,7 @@
 package mc.tsukimiya.mooney.land.domain
 
+import java.util.UUID
+
 interface LandRepository {
     companion object
 
@@ -7,11 +9,15 @@ interface LandRepository {
 
     fun find(id: LandId): Boolean
 
+    fun findByOwner(owner: UUID): Map<UUID, Land>
+
     fun findAll(): Map<LandId, Land>
 
     fun count(): Long
 
     fun store(land: Land)
+
+    fun create(coordinate: Coordinate, owner: UUID)
 
     fun delete(id: LandId)
 }
