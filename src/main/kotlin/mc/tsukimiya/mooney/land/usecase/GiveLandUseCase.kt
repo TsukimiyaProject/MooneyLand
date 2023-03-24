8c100ee0a698249b0a9fdfc10f9d6fc0ce8f9527
deceitya <49361292+deceitya@users.noreply.github.com>
2023-03-25 02:26:45 +0900

    土地をあげる

---
+++ src/main/kotlin/mc/tsukimiya/mooney/land/usecase/GiveLandUseCase.kt
@@ -0,0 +1,22 @@
+package mc.tsukimiya.mooney.land.usecase
+
+import mc.tsukimiya.mooney.land.domain.LandId
+import mc.tsukimiya.mooney.land.domain.LandRepository
+import mc.tsukimiya.mooney.land.domain.exception.InvalidOwnerException
+import mc.tsukimiya.mooney.land.domain.exception.LandNotFoundException
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.UUID
+
+class GiveLandUseCase(private val landRepository: LandRepository) {
+    fun execute(owner: UUID, target: UUID, landId: Int) {
+        transaction {
+            val land = landRepository.find(LandId(landId)) ?: throw LandNotFoundException(landId)
+            if (land.owner != owner) {
+                throw InvalidOwnerException(owner)
+            }
+
+            land.changeOwner(target)
+            landRepository.store(land)
+        }
+    }
+}
