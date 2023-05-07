7ce5ee399f2d1781992e5ca6d2542d2ceedd0d41
deceitya <49361292+deceitya@users.noreply.github.com>
2023-05-08 01:03:42 +0900

    各メソッドの実装

--- src/main/kotlin/mc/tsukimiya/mooney/land/infrastructure/repository/LandRepositoryImpl.kt
+++ src/main/kotlin/mc/tsukimiya/mooney/land/infrastructure/repository/LandRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package mc.tsukimiya.mooney.land.infrastructure.repository
 
 import mc.tsukimiya.mooney.land.domain.*
+import mc.tsukimiya.mooney.land.infrastructure.dao.Invitees
 import mc.tsukimiya.mooney.land.infrastructure.dao.Lands
 import org.jetbrains.exposed.sql.and
 import java.util.*
@@ -32,56 +33,73 @@ class LandRepositoryImpl : LandRepository {
     }
 
     override fun find(id: LandId): Land? {
-        val land = LandDao.findById(id.value) ?: return null
-        val invitee = mutableListOf<UUID>()
-        land.invitees.forEach {
-            invitee.add(it.invitee)
-        }
-        return Land(
-            id,
-            Area(Coordinate(land.minX, land.minZ, land.world), Coordinate(land.maxX, land.maxZ, land.world)),
-            land.owner,
-            invitee
-        )
+        val dao = LandDao.findById(id.value) ?: return null
+        return dao2land(dao)
     }
 
     override fun findByCoordinate(coordinate: Coordinate): Land? {
-        val land = LandDao.find {
+        val dao = LandDao.find {
             (Lands.world eq coordinate.world)
                 .and(Lands.minX greaterEq coordinate.x)
                 .and(Lands.maxX lessEq coordinate.x)
                 .and(Lands.minZ greaterEq coordinate.z)
                 .and(Lands.maxZ lessEq coordinate.z)
         }.firstOrNull() ?: return null
-        val invitee = mutableListOf<UUID>()
-        land.invitees.forEach {
-            invitee.add(it.invitee)
-        }
-        return Land(
-            LandId(land.id.value),
-            Area(Coordinate(land.minX, land.minZ, land.world), Coordinate(land.maxX, land.maxZ, land.world)),
-            land.owner,
-            invitee
-        )
+        return dao2land(dao)
     }
 
     override fun findByOwner(owner: UUID): Map<LandId, Land> {
-        TODO("Not yet implemented")
+        val lands = mutableMapOf<LandId, Land>()
+        LandDao.find {
+            Lands.owner eq owner
+        }.forEach {
+            lands[LandId(it.id.value)] = dao2land(it)
+        }
+        return lands
     }
 
     override fun findAll(): Map<LandId, Land> {
-        TODO("Not yet implemented")
+        val lands = mutableMapOf<LandId, Land>()
+        LandDao.all().forEach {
+            lands[LandId(it.id.value)] = dao2land(it)
+        }
+        return lands
     }
 
     override fun count(): Long {
-        TODO("Not yet implemented")
+        return LandDao.count()
     }
 
     override fun store(land: Land) {
-        TODO("Not yet implemented")
+        val dao = land.landId?.let { LandDao.findById(it.value) }
+        if (dao == null) {
+            LandDao.new {
+                this.owner = land.owner
+                this.minX = land.area.minCoord.x
+                this.minZ = land.area.minCoord.z
+                this.maxX = land.area.maxCoord.x
+                this.maxZ = land.area.maxCoord.z
+                this.world = land.area.getWorld()
+
+            }
+        } else {
+            dao.owner = land.owner
+            TODO("招待された/kickされたプレイヤーの永続化処理")
+        }
     }
 
     override fun delete(id: LandId) {
         TODO("Not yet implemented")
+    }
+
+    private fun dao2land(dao: LandDao): Land {
+        val invitee = mutableListOf<UUID>()
+        dao.invitees.forEach { invitee.add(it.invitee) }
+        return Land(
+            LandId(dao.id.value),
+            Area.create(dao.minX, dao.maxX, dao.minZ, dao.maxZ, dao.world),
+            dao.owner,
+            invitee
+        )
     }
 }
